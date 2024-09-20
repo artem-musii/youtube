f1b1e6500830e8cbc2ee50aaa7726f28e823cea8
Artem Musii <musiy.artem@gmail.com>
2024-09-20 23:36:20 +0200

    added jwt strategy and login/register endpoints

--- packages/youtube_backend/src/auth/auth.module.ts
+++ packages/youtube_backend/src/auth/auth.module.ts
@@ -3,17 +3,25 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from 'src/user/user.model';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, JwtStrategy],
 	imports: [
 		TypegooseModule.forFeature([
 			{
 				typegooseClass: UserModel,
 				schemaOptions: { collection: 'User' }
 			}
-		])
+		]),
+		JwtModule.registerAsync({
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
 	]
 })
 export class AuthModule {}
--- packages/youtube_backend/src/auth/auth.service.ts
+++ packages/youtube_backend/src/auth/auth.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { UserModel } from 'src/user/user.model';
+import { AuthDto } from './auth.dto';
+import { compare, genSalt, hash } from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(
+		private readonly jwtService: JwtService,
+		@InjectModel(UserModel)
+		private readonly UserModel: ModelType<UserModel>
+	) {}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto);
+
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(String(user._id))
+		};
+	}
+
+	async register(dto: AuthDto) {
+		const oldUser = await this.UserModel.findOne({ email: dto.email });
+
+		if (oldUser) {
+			throw new UnauthorizedException('User already exists');
+		}
+
+		const salt = await genSalt(10);
+
+		const newUser = new this.UserModel({
+			email: dto.email,
+			password: await hash(dto.password, salt)
+		});
+
+		const user = await newUser.save();
+
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(String(user._id))
+		};
+	}
+
+	async validateUser(dto: AuthDto) {
+		const user = await this.UserModel.findOne({ email: dto.email });
+
+		if (!user) {
+			throw new UnauthorizedException('User not found');
+		}
+
+		const isCorrectPassword = await compare(dto.password, user.password);
+
+		if (!isCorrectPassword) {
+			throw new UnauthorizedException('Invalid password');
+		}
+
+		return user;
+	}
+
+	async issueAccessToken(userId: string) {
+		return await this.jwtService.signAsync(
+			{ _id: userId },
+			{ expiresIn: '31d' }
+		);
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			email: user.email,
+			_id: user._id
+		};
+	}
+}
